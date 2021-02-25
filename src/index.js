0a67f37bdab6bd9dfdb11ee6d02f16b56b6baef5
Mir Carvalho <deppbrazil@gmail.com>
2021-02-25 13:07:11 -0300

    :rocket: add nodemon and start script

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ app.get('/', (request, response) => {
   return response.json({ message: "Hello world!" });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('✅ Backend started');
+});
