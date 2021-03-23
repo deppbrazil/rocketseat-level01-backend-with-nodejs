aa6a910d42018e1d4e19c7c6f2c458e79e104191
Mir Carvalho <deppbrazil@gmail.com>
2021-03-23 01:40:30 -0300

    :rocket: Add uuidv4 lib and update crud

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Back-end with NodeJS
 
-## Level 01
+## Notion
+-   [Cronograma](https://www.notion.so/deppbrazil/Cronograma-de-estudos-66168777b9404cc5b892b3bbe3f15175)
+-   [Start](https://www.notion.so/deppbrazil/Start-152f78ca51b0432f9ed3f88352155403)
+-   [Level 01](https://www.notion.so/deppbrazil/N-vel-01-45949675f3084a31b688f69e73e078b0)
--- src/index.js
+++ src/index.js
@@ -1,55 +1,64 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
+const projects = []
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+  const { title } = request.query;
 
-  console.log(title);
-  console.log(owner);
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
 
-  console.log(title);
-  console.log(owner);
+  const project = { id: uuid(), title, owner };
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id);
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
   const { id } = request.params;
 
-  console.log(id);
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
